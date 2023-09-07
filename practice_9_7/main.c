aa034b1e64ac5d1e6303e677b4df98ba4f94f368
qingbingwei <137413652+qingbingwei@users.noreply.github.com>
2023-09-08 07:49:49 +0800

    Add files via upload

---
+++ practice_9_7/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define ROW 9
+#define COL 9
+
+
+int main()
+{
+    //第一题
+    /*int i = 0;
+    int j = 0;
+    for(i = 1;i <= ROW;i++)
+    {
+        for(j = 1;j <= i;j++)
+        {
+            printf("%-2d*%2d = %d  ",i,j,i*j);
+        }
+        printf("\n");
+        j = 1;
+    }*/
+
+    //第二题
+    return 0;
+}
